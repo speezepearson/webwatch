4c56bbcda363911671296fe8edcbd0d5cbe4453f
Spencer Pearson <suspense@cs.washington.edu>
2015-10-31 14:11:32 -0700

    stuff

--- __init__.py
+++ __init__.py
@@ -1,15 +1,24 @@
 import http.client
 import bs4
+import datetime
+import urllib.request
 
 headers = {
   'Accept': 'text/html,application/xhtml+xml,application/xml',
   'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
 }
 
-def get_html(domain, resource='/'):
+def get_plain(domain, path, secure=False):
+  protocol = 'https' if secure else 'http'
+  return urllib.request.urlopen('{}://{}/{}'.format(protocol, domain, path)).read()
+
+def get_html(domain, path='/', secure=False):
   global headers
-  connection = http.client.HTTPConnection(domain)
-  connection.request('GET', resource, headers=headers)
+  if secure:
+    connection = http.client.HTTPSConnection(domain)
+  else:
+    connection = http.client.HTTPConnection(domain)
+  connection.request('GET', path, headers=headers)
   response = connection.getresponse()
   return response.read()
 
@@ -17,13 +26,25 @@ def soup(html):
   result = bs4.BeautifulSoup(html, 'html.parser')
   for e in list(result.find_all('script')):
     e.extract()
+  for tag in result.find_all(True):
+    for attr in list(tag.attrs.keys()):
+      if attr.startswith('on'):
+        del tag.attrs[attr]
   for e in list(result.find_all('style')):
     e.extract()
   return result
 
-def update_cache_and_return_whether_changed(cache, key, value):
-  if cache.get(key) != value:
-    cache[key] = value
-    return True
-  return False
+def update_cache_and_return_diff(cache, key, value, diff=(lambda old, new: old != new)):
+  old = cache.get(key)
+  cache[key] = value
+  return diff(old, value)
 
+def update_cache_and_return_new_rss_items(cache, name, soup):
+  items = soup.find_all('item')
+  items_by_guid = {item.guid.text: item for item in items}
+  new_guids = update_cache_and_return_diff(
+    cache, name, set(items_by_guid.keys()),
+    diff=(lambda old_guids, current_guids: current_guids if old_guids is None else current_guids - old_guids))
+  result = [items_by_guid[guid] for guid in new_guids]
+  result.sort(key=(lambda item: datetime.datetime.strptime(item.pubdate.text, '%a, %d %b %Y %H:%M:%S %z')))
+  return result
--- __main__.py
+++ __main__.py
@@ -4,35 +4,32 @@ import argparse
 import difflib
 import html
 
-from webwatch import soup, get_html, update_cache_and_return_whether_changed
+from webwatch import soup, get_html, get_plain, update_cache_and_return_diff, update_cache_and_return_new_rss_items
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--cache', default=os.path.join(os.environ['HOME'], '.fetchstuff-cache'))
 args = parser.parse_args()
 
+def submain(cache, name, soup):
+  new_items = update_cache_and_return_new_rss_items(cache, name, soup)
+  if new_items:
+    print('<h1>', name, '</h1>')
+    for item in new_items:
+      print('<article><h2><a href="{link}">{title}</a></h2></article>'.format(
+        link=item.link.text, title=item.title.text))
+      print(html.unescape(''.join(str(e) for e in item.description.contents)))
+
 with shelve.open(args.cache) as cache:
-  e = soup(get_html('xkcd.com')).select('#comic img')[0]
-  e['src'] = 'http:'+e['src']
-  if update_cache_and_return_whether_changed(cache, 'xkcd', str(e)):
-    print('<h1>xkcd</h1>')
-    print(e)
-
-  e = soup(get_html('girlgeniusonline.com', '/comic.php')).select('#comicbody img')[0]
-  if update_cache_and_return_whether_changed(cache, 'Girl Genius', str(e)):
-    print('<h1>Girl Genius</h1>')
-    print(e)
-
-  e = soup(get_html('gunnerkrigg.com')).select('.comic_image')[0]
-  for child in list(e.children):
-    child.extract()
-  e['src'] = 'http://gunnerkrigg.com'+e['src']
-  if update_cache_and_return_whether_changed(cache, 'Gunnerkrigg Court', str(e)):
-    print('<h1>Gunnerkrigg Court</h1>')
-    print(e)
+  submain(cache, 'xkcd', soup(get_html('xkcd.com', '/rss.xml')))
+  submain(cache, 'Girl Genius', soup(get_html('girlgeniusonline.com', '/ggmain.rss')))
+  submain(cache, 'Gunnerkrigg Court', soup(get_html('gunnerkrigg.com', '/rss.xml')))
+  submain(cache, 'SMBC', soup(get_html('smbc-comics.com', '/rss.php')))
+  submain(cache, 'Dinosaur Comics', soup(get_html('www.qwantz.com', '/rssfeed.php')))
+  submain(cache, 'Dr. McNinja', soup(get_plain('drmcninja.com', '/feed')))
 
   e = soup(get_html('courses.cs.washington.edu', '/courses/cse505/15au/'))
   old_cache_value = cache.get('CS 505 Autumn 2015', '')
-  if update_cache_and_return_whether_changed(cache, 'CS 505 Autumn 2015', str(e)):
+  if update_cache_and_return_diff(cache, 'CS 505 Autumn 2015', str(e)):
     print('<h1>CS 505 Autumn 2015</h1>')
     import difflib
     import html
@@ -40,3 +37,15 @@ with shelve.open(args.cache) as cache:
     new_lines = str(e).split('\n')
     diff = list(difflib.unified_diff(old_lines, new_lines))
     print('<pre>' + html.escape('\n'.join(diff)) + '</pre>')
+
+  # NOPE! Schneier is Atom, not RSS.
+  # submain(cache, 'Schneier', get_kwargs=dict(domain='www.schneier.com', path='/feed', secure=True))
+  articles = soup(get_html('www.schneier.com', secure=True)).select('.article')
+  articles_by_title = {article.select('h2.entry')[0].text: article for article in articles}
+  new_titles = update_cache_and_return_diff(
+    cache, 'Schneier', set(articles_by_title.keys()),
+    diff=(lambda old_titles, current_titles: current_titles if old_titles is None else current_titles-old_titles))
+  if new_titles:
+    print('<h1>Schneier</h1>')
+    for title in new_titles:
+      print(articles_by_title[title])
